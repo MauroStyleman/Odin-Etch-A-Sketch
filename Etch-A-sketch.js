75ceb42812fe34095655ab2f598092801c330919
Mauro <Mauro.styleman@student.kdg.be>
2022-07-28 12:36:31 +0200

    raster grootte aanpassen

--- Etch-A-sketch.js
+++ Etch-A-sketch.js
@@ -1,15 +1,24 @@
 
 function createRaster(size){
     let board = document.querySelector('#board');
-    board.style.gridTemplateColumns = `repeat(${size},1fr)`
-    board.style.gridTemplateRows  = `repeat(${size},1fr)`
+    let tile = board.querySelectorAll('div');
+    tile.forEach((div) =>div.remove());
+    board.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    board.style.gridTemplateRows  = `repeat(${size},1fr)`;
 
     for (let i = 0; i < `${size*size}`; i++){
         let tile = document.createElement('div');
-        tile.style.backgroundColor ='grey'
-        tile.style.border ='black 0.5px solid'
-        board.insertAdjacentElement('beforeend',tile)
-        board.appendChild(tile)
+        tile.style.backgroundColor ='grey';
+        tile.style.border ='black 0.5px solid';
+        board.insertAdjacentElement('beforeend',tile);
     }
 }
-createRaster(10)
+function changeSize(size){
+    if(size >=1 && size <=200) {
+        createRaster(size);
+    }
+    else {
+        alert("Size needs to be between 1 and 200");
+    }
+}
+createRaster(16)
